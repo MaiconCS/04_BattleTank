a2185f2bf6075212205e324ed079a23841a23598
Maicon C. Santana <31459524+MaiconCS@users.noreply.github.com>
2019-06-28 16:21:34 -0300

    BT21 Delegating to Components

---
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Public/TankPawn.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"//generetade is last include
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	ATankPawn* GetControlledTank()const;
+	
+};
