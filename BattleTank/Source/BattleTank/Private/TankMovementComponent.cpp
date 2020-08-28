dea1c4eab82af59eb16a639b46d1a3db623c52a6
Maicon C. Santana <31459524+MaiconCS@users.noreply.github.com>
2020-08-28 15:36:46 -0300

    BT 53 Fly-By-Wire Control System

---
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "H:\repos\04_BattleTank\BattleTank\Source\BattleTank\Public\TankMovementComponent.h"
+
---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "C:\Program Files\Epic Games\UE_4.22\Engine\Source\Runtime\Core\Public\CoreMinimal.h"
+#include "C:\Program Files\Epic Games\UE_4.22\Engine\Source\Runtime\Engine\Classes\GameFramework\NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+};
